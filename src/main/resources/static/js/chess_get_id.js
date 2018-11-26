da28af3510fc47dc241361345f9d2ee21a6107e5
Max <maxballard333@gmail.com>
2018-11-26 03:51:47 -0500

    Fix

--- src/main/resources/static/js/chess_get_id.js
+++ src/main/resources/static/js/chess_get_id.js
@@ -1,30 +1,30 @@
+function getIdChess(){
 
+    var information = {"id": $("#idInput").value};
+    console.log(information["id"]);
+    /*
+    $.ajax({
+        type : "POST",
+        contentType : "application/json",
+        url : "/ajax/getChessById",
+        data : JSON.stringify(information),
+        dataType : "json",
+        timeout : 10000,
+        success : function(data) {
+            console.log("SUCCCCESSS IT SUCKS, TO MUCH PRESS.");
+        },
+        error : function(data) {
+            console.log("ERROR BIG FELLA.");
+        },
+        done : function(e) {
+            console.log("FINISHED.tttt");
+        }
+    });
+    */
+});
 
 $(document).ready(function(){
 
 
-    $("#idSubmit").one(function(){
 
-        var information = {"id": $("#idInput").value};
-        console.log(information["id"]);
-        /*
-        $.ajax({
-            type : "POST",
-            contentType : "application/json",
-            url : "/ajax/getChessById",
-            data : JSON.stringify(information),
-            dataType : "json",
-            timeout : 10000,
-            success : function(data) {
-                console.log("SUCCCCESSS IT SUCKS, TO MUCH PRESS.");
-            },
-            error : function(data) {
-                console.log("ERROR BIG FELLA.");
-            },
-            done : function(e) {
-                console.log("FINISHED.tttt");
-            }
-        });
-        */
-    });
 });
