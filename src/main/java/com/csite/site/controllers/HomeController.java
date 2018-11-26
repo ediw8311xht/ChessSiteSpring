2ab00fc2ef6e78fda6a80d3faa86707ad975f067
Max <maxballard333@gmail.com>
2018-11-26 03:47:07 -0500

    Added chess_get_id js

--- src/main/java/com/csite/site/controllers/HomeController.java
+++ src/main/java/com/csite/site/controllers/HomeController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "home";
+        return "chess_get_id";
     }
 }
