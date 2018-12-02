b8ba76151cedb84f34e58535f8f37ab1cec543cf
Max <maxballard333@gmail.com>
2018-12-02 17:26:19 -0500

    Fixed .getBoard to properly format string

--- src/main/java/com/csite/site/engine/Game.java
+++ src/main/java/com/csite/site/engine/Game.java
@@ -29,7 +29,7 @@ public class Game {
 
     public String getBoard() {
         String g = "";
-        for (int i = 7; i > -1; i--) {
+        for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 if (board[i][j] == null)  { g += "."; }
                 else                      { g += board[i][j].to_string(); }
